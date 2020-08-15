ae30fe284cc56bc4c0cc569004fa7f3aa1a528cf
Mohammedmohasen <60930426+Mohammedmohasen@users.noreply.github.com>
2020-08-15 03:53:57 -0700

    Update orderlist.blade.php

--- resources/views/layouts/orderlist.blade.php
+++ resources/views/layouts/orderlist.blade.php
@@ -3,9 +3,9 @@
 @if (count($order))
 @foreach ($order as $item)
     <div class="well">  
-     <h3>{{$item->name}}  <a href="{{$item->id}}" class="btn list-group-item-danger">  {{$item->phone}}     </a>  {{ $item->address}}   {{$item->total}}$</h3> 
+     <h3>{{$item->name}}  <a href="{{$item->id}}" class="btn list-group-item-danger">  {{$item->phone}}     </a>  {{ $item->address}}   {{$item->total}}$  {{$item->total/3.5}}</h3> 
     </div>
   @endforeach
 @endif
-<a href="/project6/projecthome" class="btn btn-primary">GoBack</a>
+<a href="/project6/project/home" class="btn btn-primary">GoBack</a>
 @endsection
